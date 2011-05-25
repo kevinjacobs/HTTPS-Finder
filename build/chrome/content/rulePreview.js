594752f8f2129de0b06c04daddd92cc55fa38957
Kevin Jacobs <kevin.a.jacobs@hotmail.com>
2011-05-25 03:33:00 +0000

    0.67 + built XPI

--- build/chrome/content/rulePreview.js
+++
@@ -1,8 +0,0 @@
-function httpsfinderLoadRulePreview(doc){
-    document.getElementById('ruleBox').value = window.arguments[0].inn.rule;
-}
-
-function httpsfinderOkRulePreview(doc){
-    window.arguments[0].out = { rule:document.getElementById("ruleBox").value};
-    
-}
--- build/chrome/defaults/preferences/foxbeansdef.js
+++
@@ -1,2 +0,0 @@
-pref("extensions.{6bdc61ae-7b80-44a3-9476-e1d121ec2238}.description", "chrome://httpsfinder/locale/description.properties");  
-
